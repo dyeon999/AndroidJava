24dc5f4b5fb283860308e2c8071435cc6cd37521
dobby <dobbidd0@gmail.com>
2023-06-30 17:27:17 +0900

    2023.06.30 ??! #10926

---
+++ Bronse/src/surprised.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+// 10926
+public class surprised {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        System.out.println(input + "??!");
+    }
+}
