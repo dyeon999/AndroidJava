61e18c6397944a065b0a3cc51978d64a39955068
dobby <dobbidd0@gmail.com>
2023-07-03 22:54:08 +0900

    2023.07.03 태국에서는 어쩌구?! #18108

---
+++ Bronse/src/thailand.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+// 18108
+public class thailand {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        System.out.println(y - 543);
+    }
+}
