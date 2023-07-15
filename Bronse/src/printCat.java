a2bcf1d324e5273990cc68dedb00a06f3efdd7c9
dobby <dobbidd0@gmail.com>
2023-07-15 21:05:10 +0900

    2023.07.15 고양이 #10171

---
+++ Bronse/src/printCat.java
@@ -0,0 +1,14 @@
+// 10171
+public class printCat {
+    public static void main(String[] args) {
+        String cat1 = "\\    /\\";
+        String cat2 = " )  ( \')";
+        String cat3 = "(  /  )";
+        String cat4 = " \\(__)|";
+
+        System.out.println(cat1);
+        System.out.println(cat2);
+        System.out.println(cat3);
+        System.out.println(cat4);
+    }
+}
