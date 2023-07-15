162f7918853f49429266dd18e811ce227e9e9514
dobby <dobbidd0@gmail.com>
2023-07-15 21:09:43 +0900

    2023.07.15 개 #10172

---
+++ Bronse/src/printDog.java
@@ -0,0 +1,13 @@
+public class printDog {
+    public static void main(String[] args) {
+        String dog = "|\\_/| \n|q p|   /} \n( 0 )\"\"\"\\ \n|\"^\"`    | \n||_/=\\\\__|";
+
+        System.out.println(dog);
+    }
+}
+
+// |\_/|
+// |q p|   /}
+// ( 0 )"""\
+// |"^"`    |
+// ||_/=\\__|
