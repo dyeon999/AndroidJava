c601a75d26a7841286535d2f65159710c4951e6f
dobby <dobbidd0@gmail.com>
2023-07-15 20:57:38 +0900

    2023.07.15 꼬마정민 #11382

---
+++ Bronse/src/littleJM.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+// 11382
+public class littleJM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Long a = sc.nextLong();
+        Long b = sc.nextLong();
+        Long c = sc.nextLong();
+
+        Long sum = a + b + c;
+        System.out.println(sum);
+    }
+}
