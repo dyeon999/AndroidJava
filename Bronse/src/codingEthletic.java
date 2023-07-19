d32c68b7a17a3f570190c36e6fbcded736f2edd0
dobby <dobbidd0@gmail.com>
2023-07-19 23:29:08 +0900

    2023.07.19 코딩은 체육과목입니다 #25314

---
+++ Bronse/src/codingEthletic.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// 25314
+public class codingEthletic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for(int i = 0; i < n/4; i++){
+            System.out.printf("long ");
+        }
+        System.out.println("int");
+    }
+}
