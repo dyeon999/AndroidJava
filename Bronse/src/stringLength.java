1594840f8d70f66be71d3a1e9f87b0a4f6fc6d6b
dobby <dobbidd0@gmail.com>
2023-07-21 23:48:20 +0900

    2023.07.21 문자열 길이 #2743

---
+++ Bronse/src/stringLength.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+// 2743
+public class stringLength {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        
+        System.out.println(str.length());
+    }
+}
