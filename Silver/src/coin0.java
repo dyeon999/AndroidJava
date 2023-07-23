5b4ddab825c6f58805b3ee886dc2b29ec19c5945
dobby <dobbidd0@gmail.com>
2023-07-23 22:26:43 +0900

    2023.07.23 동전0 #11047

---
+++ Silver/src/coin0.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class coin0 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cnt = 0;
+        int n = sc.nextInt();
+        int total = sc.nextInt();
+        int[] list = new int[n];
+
+        for(int i = 0; i < n; i++){
+            list[n - i - 1] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++){
+            cnt += total / list[i];
+            total = total % list[i];
+        }
+
+        System.out.println(cnt);
+    }
+}
