cef0de6fcdb49431ee9a4d8d0761510ebbba2ab7
dobby <dobbidd0@gmail.com>
2023-08-07 21:33:19 +0900

    2023.08.07 블랙잭 #2798

---
+++ Bronse/src/blackJack.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+// 2798
+public class blackJack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] cardList = new int[n];
+
+        for (int i = 0; i < n; i++){
+            cardList[i] = sc.nextInt();
+        }
+
+        int max = 0;
+
+        for (int i = 0; i < n-2; i++){
+            for (int j = i+1; j < n-1; j++){
+                for(int k = j+1; k < n; k++){
+                    int sum = cardList[i] + cardList[j] + cardList[k];
+                    if (sum <= m){
+                        max = Math.max(max, sum);
+                    }
+                    if (max == m) {
+                        System.out.println(max);
+                        return;
+                    }
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
