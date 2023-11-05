cb1f6a4fe0df6a14bcc7f5105ff2de0c4f2551cf
dobby <dobbidd0@gmail.com>
2023-11-05 19:20:02 +0900

    다리

--- Silver/src/buildBridge.java
+++ Silver/src/buildBridge.java
@@ -1,5 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class buildBridge {
-    public static void main(String[] args) {
-        
+    static int[][] bridge = new int[30][30];
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(bf.readLine());
+        int n, m = 0;
+
+        for (int i = 0; i < t; i++){
+            String[] input = bf.readLine().split(" ");
+            n = Integer.parseInt(input[0]);
+            m = Integer.parseInt(input[1]);
+
+            System.out.println(combination(m, n));
+        }
     }
+
+    private static int combination(int n, int r){
+        if (bridge[n][r] > 0){
+            return bridge[n][r];
+        } else if (n == r || r == 0){
+            bridge[n][r] = 1;
+            return bridge[n][r];
+        } else{
+            return combination(n-1, r-1) + combination(n-1, r);
+        }
+
+    }
+
 }
